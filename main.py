702abb4a59e407ef305d89d46e45bf67740759f2
nazwa uzytkownika <143868818+KremikEmik@users.noreply.github.com>
2025-04-29 14:12:05 +0200

    kod 29.04

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ class MyForm(QDialog):
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
 
+        self.liczby = []
         self.ui.wczytajButton.clicked.connect(self.wczytaj)
         self.ui.generujButton.clicked.connect(self.generuj)
 
@@ -22,15 +23,60 @@ class MyForm(QDialog):
         if ok:
             liczba = [randint(0,100) for i in range(int(n))]
 
-        self.ui.liczbaEdit.setText(str(n))
-        self.ui.liczbyEdit.setText(str(liczba))
+        self.ui.liczbaEdit.setText(str(len(self.liczby)))
+        self.ui.liczbyEdit.setText(str(self.liczby))
+        self.uzupelnij()
 
     def wczytaj(self):
         sciezka, _ = QFileDialog.getOpenFileName(self, 'Wczytaj plik', '.', 'Text files (*.csv)')
-        with open(sciezka, 'r') as plik:
-            self.ui.liczbyEdit.setPlainText(plik.read())
+        if sciezka:
+            with open(sciezka, 'r') as plik:
+                zawartosc = plik.read()
+                elementy = zawartosc.split(';')
+                self.liczby = [int(element) for element in elementy]
+                self.ui.liczbyEdit.setText(str(self.liczby))
+        self.ui.liczbaEdit.setText(str(len(self.liczby)))
+        self.uzupelnij()
 
+    def srednia_arytmetyczna(self):
+        return sum(self.liczby) / len(self.liczby)
 
+    def srednia_geometryczna(self):
+        iloczyn = 1
+        for liczby in self.liczby:
+            iloczyn *= liczby
+        return iloczyn ** (1 / len(self.liczby)) #pierwiastek n-tego stopnia jako a do 1/n
+
+    def uzupelnij(self):
+        self.ui.arytmetycznaEdit.setText(f'{self.srednia_arytmetyczna():.2f}')
+        self.ui.geometrycznaEdit.setText(f'{self.srednia_geometryczna():.2f}')
+        self.ui.medianaEdit.setText(f'{self.mediana():.2f}')
+        self.ui.dominantaEdit.setText(str(self.dominanta()))
+
+    def mediana(self):
+        dlugosc = len(self.liczby)
+        liczby_posortowane = sorted(self.liczby)
+        if dlugosc % 2 == 1: #nieparzysta
+            return liczby_posortowane[dlugosc // 2]
+        else:
+            return (liczby_posortowane[dlugosc // 2 - 1] + liczby_posortowane[dlugosc // 2]) / 2
+
+    def dominanta(self):
+        wystapienia = {}
+        for liczba in self.liczby:
+            if liczba not in wystapienia.keys():
+                wystapienia[liczba] = 1
+            else:
+                wystapienia[liczba] += 1
+
+        max = 0
+        max_argument = 0
+        for klucz, wartosc in wystapienia.items(): #zwraca klucz wartosci
+            if wartosc > max:
+                max = wartosc
+                max_argument = klucz
+
+        return max_argument
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
